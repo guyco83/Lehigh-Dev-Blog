daf64be7b47bd56827eaaf039caa537446a35879
J-Katzen <Alto50@gmail.com>
2012-06-05 23:57:11 -0400

    Started a RoR app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LehighDevSite::Application.config.secret_token = 'cc5050174ed156b515c0716a0bdb144de2adc1eb17de2dbcaef625d865c1da34bc9b0ed267d6f5d1715b351f9114ae0ae323cc1e3e01845db0943c47d6961681'
